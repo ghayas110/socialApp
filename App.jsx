aadf73c198ee92758b611fcad602aabac857c101
Ghayas Ali <55618250+ghayas110@users.noreply.github.com>
2024-05-28 19:06:58 +0500

    sss

--- App.jsx
+++ App.jsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MainNavigation from './navigation/MainNavigation'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <>
+<MainNavigation/>
+    </>
   )
 }
 
--- pages/SplashScreen.jsx
+++ pages/SplashScreen.jsx
@@ -1,14 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import { StyleSheet, Text, View,Image } from 'react-native'
+import React,{useEffect} from 'react'
+import { useNavigation } from '@react-navigation/native';
 const SplashScreen = () => {
-  return (
-    <View>
-      <Text>SplashScreen</Text>
+    const navigation =useNavigation()
+    useEffect(() => {
+      setTimeout(() => {
+        navigation.navigate('Login')
+      }, 1000); // Navigate to Home screen after 10 seconds
+    }, [navigation]);
+  
+    return (
+      <View style={styles.container}>
+   <Image source={require('../images/logo.jpeg')} style={{width:200,height:220,resizeMode:'contain'}}/>
+   
     </View>
-  )
-}
+  );
+};
 
 export default SplashScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+     
+      },
+})
